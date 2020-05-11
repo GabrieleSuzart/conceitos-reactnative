1190db173ccef9509720bcac6cb9fe25e9122a3f
GabrieleSuzart <gabrielesuzart@gmail.com>
2020-05-11 01:39:26 -0300

    List and like repositories

--- src/services/api.js
+++ src/services/api.js
@@ -5,3 +5,12 @@ const api = axios.create({
 });
 
 export default api;
+
+/**
+* iOS com emulador: localhost
+* iOS com device físico: IP da máquina
+* Android com device físico: IP da máquina
+* Android com emulador: para usar o localhost com o emulador aberto rodar `adb reverse tcp:3333 tcp:3333`
+* Android com emulador: 10.0.2.2 (Android studio)
+* Android com emulador: 10.0.3.2 (Genymotion)
+*/
